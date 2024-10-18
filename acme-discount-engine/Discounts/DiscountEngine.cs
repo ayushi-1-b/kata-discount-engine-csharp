333a06e9741d3f9f4880e2b222e4e3b288d8c53d
ayushi-1-b <ayushi.badiyani@bjss.com>
2024-10-18 14:55:31 +0100

    Split the apply discount function into seperate functions

--- acme-discount-engine/Discounts/DiscountEngine.cs
+++ acme-discount-engine/Discounts/DiscountEngine.cs
@@ -3,14 +3,14 @@
 namespace acme_discount_engine.Discounts
 {
     public class DiscountEngine
-    {
+    { 
         public bool LoyaltyCard { get; set; }
         public DateTime Time { get; set; } = DateTime.Now;
 
         private List<string> TwoForOneList = new List<string> { "Freddo" };
         private List<string> NoDiscount = new List<string> { "T-Shirt", "Keyboard", "Drill", "Chair" };
 
-        public double ApplyDiscounts(List<Item> items)
+        public void ApplyTwoForOneDiscount(List<Item> items)
         {
             items.Sort((x, y) => x.Name.CompareTo(y.Name));
             string currentItem = string.Empty;
@@ -33,58 +33,55 @@ namespace acme_discount_engine.Discounts
                     }
                 }
             }
+        }
 
-            double itemTotal = 0.00;
-            foreach (var item in items)
+        public void ApplyPerishableItemDiscount(Item item, int daysUntilDate)
+        {
+            if (daysUntilDate == 0)
             {
-                itemTotal += item.Price;
-                int daysUntilDate = (item.Date - DateTime.Today).Days;
-                if(DateTime.Today > item.Date) { daysUntilDate = -1; }
-
-                if (!item.IsPerishable)
+                if (Time.Hour >= 0 && Time.Hour < 12)
                 {
-                    if (!NoDiscount.Contains(item.Name))
-                    {
-                        if (daysUntilDate >= 6 && daysUntilDate <= 10)
-                        {
-                            item.Price -= item.Price * 0.05;
-                        }
-                        else if (daysUntilDate >= 0 && daysUntilDate <= 5)
-                        {
-                            item.Price -= item.Price * 0.10;
-                        }
-                        else if (daysUntilDate < 0)
-                        {
-                            item.Price -= item.Price * 0.20;
-                        }
-                    }
+                    item.Price -= item.Price * 0.05;
                 }
-                else
+                else if (Time.Hour >= 12 && Time.Hour < 16)
                 {
-                    if (daysUntilDate == 0)
-                    {
-                        if (Time.Hour >= 0 && Time.Hour < 12)
-                        {
-                            item.Price -= item.Price * 0.05;
-                        }
-                        else if (Time.Hour >= 12 && Time.Hour < 16)
-                        {
-                            item.Price -= item.Price * 0.10;
-                        }
-                        else if (Time.Hour >= 16 && Time.Hour < 18)
-                        {
-                            item.Price -= item.Price * 0.15;
-                        }
-                        else if (Time.Hour >= 18)
-                        {
-                            item.Price -= item.Price * (!item.Name.Contains("(Meat)") ? 0.25 : 0.15);
-                        }
-                    }
+                    item.Price -= item.Price * 0.10;
+                }
+                else if (Time.Hour >= 16 && Time.Hour < 18)
+                {
+                    item.Price -= item.Price * 0.15;
+                }
+                else if (Time.Hour >= 18)
+                {
+                    item.Price -= item.Price * (!item.Name.Contains("(Meat)") ? 0.25 : 0.15);
+                }
+            }
+        }
+
+        public void ApplyNonPerishableItemDiscount(Item item, int daysUntilDate)
+        {
+            if (!NoDiscount.Contains(item.Name))
+            {
+                if (daysUntilDate >= 6 && daysUntilDate <= 10)
+                {
+                    item.Price -= item.Price * 0.05;
+                }
+                else if (daysUntilDate >= 0 && daysUntilDate <= 5)
+                {
+                    item.Price -= item.Price * 0.10;
+                }
+                else if (daysUntilDate < 0)
+                {
+                    item.Price -= item.Price * 0.20;
                 }
             }
 
-            currentItem = string.Empty;
-            itemCount = 0;
+        }
+
+        public void ApplyBulkDiscounts(List<Item> items)
+        {
+            string currentItem = string.Empty;
+            int itemCount = 0;
             for (int i = 0; i < items.Count; i++)
             {
                 if (items[i].Name != currentItem)
@@ -105,12 +102,41 @@ namespace acme_discount_engine.Discounts
                     }
                 }
             }
+        }
+
+        public double ApplyLoyaltyCardDiscount(double total)
+        {
+            total -= total * 0.02;
+            return total;
+        }
+
+        public double ApplyDiscounts(List<Item> items)
+        {
+            ApplyTwoForOneDiscount(items);
+            double itemTotal = 0.00;
+            foreach (var item in items)
+            {
+                itemTotal += item.Price;
+                int daysUntilDate = (item.Date - DateTime.Today).Days;
+                if(DateTime.Today > item.Date) { daysUntilDate = -1; }
+
+                if (!item.IsPerishable)
+                {
+                    ApplyNonPerishableItemDiscount(item,daysUntilDate);
+                }
+                else
+                {
+                    ApplyPerishableItemDiscount(item, daysUntilDate);
+                }
+            }
+
+            ApplyBulkDiscounts(items);
 
             double finalTotal = items.Sum(item => item.Price);
 
             if (LoyaltyCard && itemTotal >= 50.00)
             {
-                finalTotal -= finalTotal * 0.02;
+                finalTotal = ApplyLoyaltyCardDiscount(finalTotal);
             }
 
             return Math.Round(finalTotal, 2);
