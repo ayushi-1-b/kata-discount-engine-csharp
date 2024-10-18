c83f3d6d85c68bb5c76ab565ad5172c95b68b3dd
ayushi-1-b <ayushi.badiyani@bjss.com>
2024-10-18 16:43:02 +0100

    Extracted discounts for perishable and non perishable items into their own classes based on item interface that implements quantity as well

---
+++ acme-discount-engine/Discounts/NonPerishableItem.cs
@@ -0,0 +1,42 @@
+﻿using AcmeSharedModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acme_discount_engine.Discounts
+{
+    internal class NonPerishableItem : DiscountEngineItem
+    {
+        public Item item { get; set; }
+        public int Quantity { get; set; } = 0;
+
+        public void ApplyDiscount(List<string> NoDiscount)
+        {
+            int daysUntilDate = (item.Date - DateTime.Today).Days;
+            if (DateTime.Today > item.Date) { daysUntilDate = -1; }
+
+            if (!NoDiscount.Contains(item.Name))
+            {
+                if (daysUntilDate >= 6 && daysUntilDate <= 10)
+                {
+                    item.Price -= item.Price * 0.05;
+                }
+                else if (daysUntilDate >= 0 && daysUntilDate <= 5)
+                {
+                    item.Price -= item.Price * 0.10;
+                }
+                else if (daysUntilDate < 0)
+                {
+                    item.Price -= item.Price * 0.20;
+                }
+            }
+        }
+
+        public NonPerishableItem(Item item) 
+        {
+            this.item = item;
+        }
+    }
+}
---
+++ acme-discount-engine/Discounts/PerishableItem.cs
@@ -0,0 +1,46 @@
+﻿using AcmeSharedModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace acme_discount_engine.Discounts
+{
+    internal class PerishableItem : DiscountEngineItem
+    {
+        public Item item { get; set; }
+        public int Quantity { get ; set; } = 0;
+        public DateTime Time { get; set; } = DateTime.Now;
+        public void ApplyDiscount(List<string> NoDiscount)
+        {
+            int daysUntilDate = (item.Date - DateTime.Today).Days;
+            if (DateTime.Today > item.Date) { daysUntilDate = -1; }
+
+            if (daysUntilDate == 0)
+            {
+                if (Time.Hour >= 0 && Time.Hour < 12)
+                {
+                    item.Price -= item.Price * 0.05;
+                }
+                else if (Time.Hour >= 12 && Time.Hour < 16)
+                {
+                    item.Price -= item.Price * 0.10;
+                }
+                else if (Time.Hour >= 16 && Time.Hour < 18)
+                {
+                    item.Price -= item.Price * 0.15;
+                }
+                else if (Time.Hour >= 18)
+                {
+                    item.Price -= item.Price * (!item.Name.Contains("(Meat)") ? 0.25 : 0.15);
+                }
+            }
+        }
+
+        public PerishableItem(Item item) 
+        {
+            this.item = item;
+        }
+    }
+}
